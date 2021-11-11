973ba889de245ed91b5d42c414049eb99047452f
afolz121 <50084625+afolz121@users.noreply.github.com>
2021-11-11 17:19:01 -0600

    second commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Andrew Folz on 11/9/21.
+//
+
+import SwiftUI
+
+// struct short for data structure
+// or a collection of variables.
+// you can also have functions in structs.
+
+// view means it behaves like a View struct
+/* All View structs have a function called .padding()
+    padding means create a boundary around the text
+    padding returns something that behaves like a View
+    or a padded modified View. It DOES NOT return text.
+    
+ */
+
+struct CardView: View {
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 25.0)
+                .stroke(lineWidth: 3.0)
+                .foregroundColor(Color/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+            Text("✈️")
+                .foregroundColor(Color.blue)
+                .font(.largeTitle)
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            CardView()
+                
+            CardView()
+            CardView()
+            CardView()
+        }
+            .padding(.horizontal)
+    }
+}
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,23 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Andrew Folz on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct MemorizeApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
